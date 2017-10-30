ae11e0176b5725b1aa61500e254963fd743e3456
Rumhocker <rumhocker@deutsche-emark.org>
2017-10-30 20:00:01 +0100

    accept blocks with unknown version

--- src/main.cpp
+++ src/main.cpp
@@ -2161,8 +2161,10 @@ bool CBlock::AcceptBlock()
 {
     AssertLockHeld(cs_main);
 
-    if (nVersion > CURRENT_VERSION)
-        return DoS(100, error("AcceptBlock() : reject unknown block version %d", nVersion));
+    if (!IsProtocolV3(nTime)) {
+        if (nVersion > CURRENT_VERSION)
+            return DoS(100, error("AcceptBlock() : reject unknown block version %d", nVersion));
+    }
 
     // Check for duplicate
     uint256 hash = GetHash();
