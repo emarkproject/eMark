39c8f11441ef5b79a983d980f01c918599a507aa
Rumhocker <rumhocker@deutsche-emark.org>
2017-11-16 22:50:29 +0100

    Qt: change Address QR-code to make readable with Android Wallet

--- src/qt/qrcodedialog.cpp
+++ src/qt/qrcodedialog.cpp
@@ -83,7 +83,7 @@ void QRCodeDialog::genCode()
 
 QString QRCodeDialog::getURI()
 {
-    QString ret = QString("eMark:%1").arg(address);
+    QString ret = QString("%1").arg(address);
     int paramCount = 0;
 
     ui->outUri->clear();
