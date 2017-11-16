14f8a17ece1847e31e09823027d48d556f7d4591
Rumhocker <rumhocker@deutsche-emark.org>
2017-11-16 18:26:44 +0100

    bump version

--- src/version.cpp
+++ src/version.cpp
@@ -37,7 +37,7 @@ const std::string CLIENT_NAME("eMark");
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
 #    define GIT_COMMIT_ID ""
-#    define GIT_COMMIT_DATE "29.04.2016"
+#    define GIT_COMMIT_DATE "16.11.2017"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
