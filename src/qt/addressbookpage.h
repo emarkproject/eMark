cd2bd6e7a1f7ffe7a94fa7effa0ab8c17dc6ec7e
Rumhocker <rumhocker@deutsche-emark.org>
2017-11-16 22:39:11 +0100

    Qt: cleanup / optimise addressbookpage
    - don't show QR Code context menu, when USE_QRCODE=1 was not specified
    when compiling the client
    - re-work on_showQRCode_clicked() for better readability and remove an
    unneeded duplicate check
    - re-work on_signMessage_clicked() and on_verifyMessage_clicked() to
    match foreach in on_showQRCode_clicked(), which seems more robust /
    cleaner
    - re-order context menu stuff to match real context menu layout
    - add comments for all private slots in the class

--- src/qt/addressbookpage.h
+++ src/qt/addressbookpage.h
@@ -58,22 +58,26 @@ private:
     QString newAddressToSelect;
 
 private slots:
-    void on_deleteButton_clicked();
+    /** Delete currently selected address entry */
+	void on_deleteButton_clicked();
+	/** Create a new address for receiving coins and / or add a new address book entry */
     void on_newAddressButton_clicked();
     /** Copy address of currently selected address entry to clipboard */
     void on_copyToClipboard_clicked();
+	/** Open the sign message tab in the Sign/Verify Message dialog with currently selected address */
     void on_signMessage_clicked();
+	/** Open the verify message tab in the Sign/Verify Message dialog with currently selected address */
     void on_verifyMessage_clicked();
-    void selectionChanged();
+	/** Generate a QR Code from the currently selected address */
     void on_showQRCode_clicked();
-    /** Spawn contextual menu (right mouse menu) for address book entry */
-    void contextualMenu(const QPoint &point);
-
     /** Copy label of currently selected address entry to clipboard */
     void onCopyLabelAction();
     /** Edit currently selected address entry */
     void onEditAction();
-
+    /** Set button states based on selected tab and selection */
+    void selectionChanged();
+    /** Spawn contextual menu (right mouse menu) for address book entry */
+    void contextualMenu(const QPoint &point);
     /** New entry/entries were added to address table */
     void selectNewAddress(const QModelIndex &parent, int begin, int end);
 
