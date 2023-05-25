3953d59ea402bd8b5d4c431fa1bf5f819bcfeceb
dbkeys <dbkeys@users.noreply.github.com>
2023-05-25 11:44:02 -0700

    Revert "Initial work on implementing BIP65 OP_LOCKCHECKTIMEVERIFY"

--- src/core.h
+++ src/core.h
@@ -1,9 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2013 The Bitcoin developers
-// Modified Code: 2023 eMark developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 #ifndef BITCOIN_CORE_H
 #define BITCOIN_CORE_H
 
